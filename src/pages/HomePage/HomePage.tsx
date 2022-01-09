3ddce5077955ae298847d579e6b6fce598b792ba
Nahuel Castro <nahukas@gmail.com>
2022-01-09 18:11:11 +0100

    First config

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>Hello World!</div>
+    </div>
+  </div>
+);
+
+export default HomePage;
