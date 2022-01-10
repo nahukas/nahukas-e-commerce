08992b4e6d3e4d3a892c8a837a09ab5f2147c4af
Nahuel Castro <nahukas@gmail.com>
2022-01-10 16:16:16 +0100

    Finish header

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from '../components/header/Header';
 import HomePage from '../pages/HomePage/HomePage';
 import ShopPage from '../pages/ShopPage/ShopPage';
 
 const RoutesComponent = () => (
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/shop' element={<ShopPage />} />
